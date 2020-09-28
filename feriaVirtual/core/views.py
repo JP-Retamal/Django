9ac1135b6522f47f5b8cb1f66c3b2092628776d1
JP reta <jp_reta@hotmail.es>
2020-09-28 00:34:37 -0400

    buscar detalle listo

--- feriaVirtual/core/views.py
+++ feriaVirtual/core/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.db import connection
 from datetime import date
+import cx_Oracle
 # Create your views here. la funcion def home busca el template (controlador)
 
 def home(request):
     tituloPagina = 'Inicio'
-    return render(request, 'index.html', { 'tituloPagina' : tituloPagina })
+    data = {
+        'saldos':listar_saldos()
+    }
+    return render(request, 'index.html', { 'tituloPagina' : tituloPagina, 'data' : data })
 
 def login(request):
     tituloPagina = 'Ingreso'
@@ -18,17 +22,37 @@ def registro(request):
     data = {
         'regiones':listar_por_regiones()
     }
-    return render(request, 'registro.html', { 'fechaActual' : fechaActual, 'tituloPagina' : tituloPagina , 'data':data})
 
-def detalle(request):#detalle_id
-   # print(listar_detallesaldos(detalle_id))
-    tituloPagina = 'Manzana Candy'
-    precio = '{:,}'.format(1990).replace(',','.')
-    cantidad = '1'
-    return render(request, 'detalle.html', { 'tituloPagina' : tituloPagina, 'precio' : precio, 'cantidad' : cantidad })
+    if request.method == 'POST':
+        ru = request.POST.get('registro-rut')
+        nombre = request.POST.get('registro-nombre')
+        paterno = request.POST.get('registro-Paterno')
+        materno = request.POST.get('registro-materno')
+        f_nac = request.POST.get('registro-fecha')
+        email = request.POST.get('registro-correo')
+        direccion = request.POST.get('registro-direccion')
+        celular = request.POST.get('registro-celular')
+        clave = request.POST.get('registro-rut')
+        comuna = request.POST.get('registro-comuna')
+        salida = agregar_comerciante(ru, nombre, paterno, materno, f_nac, email, direccion, celular, clave, comuna)
+        if salida == 1:
+            data['mensaje'] = 'Agregado correctamente'
+        else:
+            data['mensaje'] = 'El registro no se agregó'
+
+    return render(request, 'registro.html', { 'fechaActual' : fechaActual, 'tituloPagina' : tituloPagina , 'data':data})
+    
+    
+def detalle(request,detalle_id):#detalle_id
+    #print(listar_detallesaldos(detalle_id))
+    data = {
+        'db_vlocal':listar_detallesaldos(detalle_id)
+    }
+  
+    return render(request, 'detalle.html', data)
 
 def portalSaldos(request):
-    print(listar_saldos())#ver listado en consola
+    #print(listar_saldos())#ver listado en consola
     data = {
         'saldos':listar_saldos()
     }
@@ -66,10 +90,32 @@ def listar_detallesaldos(detalle_id):
     cursor = django_cursor.connection.cursor()
     out_cur = django_cursor.connection.cursor()
 
-    cursor.callproc("SP_BUSCAR_DETALLE_VLOCAL", [out_cur])
+    cursor.callproc("SP_BUSCAR_DETALLE_VLOCAL", [detalle_id, out_cur])
 
     lista = []
     for fila in out_cur:
         lista.append(fila)
 
     return lista  
+
+
+def agregar_comerciante(RUN_USUARIO, NOMBRE, AP_PATERNO, AP_MATERNO, FECHA_NAC, EMAIL, DIRECCION, NUM_CELULAR, CLAVE, ID_COMUNA):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    ID_ESTADO =1 
+    ID_ROL=3
+    ID_GENERO =1
+    ID_EMPRESA =0
+    cursor.callproc('SP_REGISTRO_COMERCIANTES', [ RUN_USUARIO, NOMBRE, AP_PATERNO, AP_MATERNO, FECHA_NAC, EMAIL, DIRECCION, NUM_CELULAR, CLAVE, ID_ESTADO, ID_COMUNA, ID_ROL, ID_GENERO, ID_EMPRESA])
+    return salida.getvalue()
+
+#def detalle(request,detalle_id):#detalle_id
+  #  print(listar_detallesaldos(detalle_id))
+   # data = {
+   #     'BD_VLOCAL':listar_detallesaldos(detalle_id)
+  #  }
+  #  tituloPagina = 'Manzana Candy'
+  #  precio = '{:,}'.format(1990).replace(',','.')
+  #  cantidad = '1'
+   # return render(request, 'detalle.html', { 'tituloPagina' : tituloPagina, 'precio' : precio, 'cantidad' : cantidad })
